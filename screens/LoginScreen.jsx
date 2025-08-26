d462c686a75a9f98edb8b7e3c76ec54af196a88c
lalitsingh789 <singhlalit789.w017@gmail.com>
2025-08-27 00:22:29 +0530

    up

--- screens/LoginScreen.jsx
+++ screens/LoginScreen.jsx
@@ -26,7 +26,7 @@ const LoginScreen = ({ navigation }) => {
 
     setLoading(true);
     try {
-      const res = await fetch('http://192.168.56.183:5000/api/auth/login', {
+      const res = await fetch('https://clinic-3l8a.onrender.com/api/auth/login', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ email, password }),
