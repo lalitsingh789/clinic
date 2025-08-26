44edb81d39072f9db872c269367a562a40ddbc51
lalitsingh789 <singhlalit789.w017@gmail.com>
2025-08-26 13:07:33 +0530

    mistake

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const cors = require('cors');
 const app = express();
 
 require('dotenv').config(); 
-const connectDB = require('./connectDB');
+const connectDB = require('./config/db');
 connectDB();
 
 app.use(cors());
